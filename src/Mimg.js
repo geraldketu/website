d7544c101b61e716eda85c60f4737e5dc65b2cd2
geraldketu <93153805+geraldketu@users.noreply.github.com>
2023-02-16 09:52:37 -0500

    Update Mimg.js

--- src/Mimg.js
+++ src/Mimg.js
@@ -4,7 +4,7 @@ import './App.css'
 export default function Mimg() {
   return (
     <div className ="Mimg">
-      <img src="my_web4\public\665537C3-878A-4492-9289-3DD82B91F56D (1).jpeg" alt=""/>
+      <img src="665537C3-878A-4492-9289-3DD82B91F56D (1).jpeg" alt=""/>
     </div>
   )
 }

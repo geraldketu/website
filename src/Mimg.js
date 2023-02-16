83716260e1aff019c76cebf714c133afbd13cad7
geraldketu <93153805+geraldketu@users.noreply.github.com>
2023-02-16 12:00:19 -0500

    Update Mimg.js

--- src/Mimg.js
+++ src/Mimg.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import './App.css'
+import photo from './665537C3-878A-4492-9289-3DD82B91F56D (1).jpeg';
 
 export default function Mimg() {
   return (
     <div className ="Mimg">
-      <img src="665537C3-878A-4492-9289-3DD82B91F56D (1).jpeg" alt=""/>
+      <img src={photo} alt=""/>
     </div>
   )
 }

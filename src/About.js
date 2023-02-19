4c156b929aeeba82cf322aedba1c0c855bc49bf9
Your Name <you@example.com>
2023-02-18 23:04:54 -0500

    Edited image file

--- src/About.js
+++ src/About.js
@@ -8,7 +8,7 @@ function About (){
     
     return (
 <div className ="About">
-<p4>{"Welcome to my personal website! My name is Gerald Ndawula, and I am currently a computer science student at Montgomery College. I am proficient in programming languages such as C++, Java, and JavaScript, and have experience in developing a wide range of software applications. On my website, you will find information about my education, projects, and experience, as well as my contact information for networking and job opportunities. Thank you for visiting!"}</p4>
+<p4>{"Wlcome to my personal website! My name is Gerald Ndawula, and I am currently a computer science student at Montgomery College. I am proficient in programming languages such as C++, Java, and JavaScript, and have experience in developing a wide range of software applications. On my website, you will find information about my education, projects, and experience, as well as my contact information for networking and job opportunities. Thank you for visiting!"}</p4>
 </div>
     );
 }
